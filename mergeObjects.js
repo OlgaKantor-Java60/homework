41b48e7be08d45a44f8ab26878b906f3ef48fe9b
OlgaKantor <kntrolga@gmail.com>
2025-03-07 15:37:32 +0200

    first solution of mergeObjects

--- mergeObjects.js
+++ mergeObjects.js
@@ -1,7 +1,16 @@
 export function mergeObjects(firstObj, secondObj, isFirstPref) {
-    //TODO
     //returns result object as merging of firstObj and secondObj
     //if there is conflict of keys the parameter isFirstPref defines how to resove a conflict
     //if isFirstPref is true, then result object should contain value of the conflicted key from the first object
     //otherwise - from the second one
+
+let res = {};
+if (isFirstPref) {
+ res = {...secondObj, ...firstObj}
+} else {
+ res = {...firstObj, ...secondObj};
 }
+return res;
+} 
+
+//doesn't pass the test, but consits the same object (?)
